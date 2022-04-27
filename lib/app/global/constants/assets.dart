e847e7f877e771fb7c8a9d4f42413109da4caba1
GabrielUlisses <gulissessti@hotmail.com>
2022-04-26 21:24:40 -0300

    create the global package with commom and constants folders, create the Asset manager class

---
+++ lib/app/global/constants/assets.dart
@@ -0,0 +1,26 @@
+class Assets{
+  const Assets();
+
+  static const images = Images();
+  static const icons = Icons;
+
+}
+
+class Images{
+  const Images();
+
+  final String flagUnitedStates = "assets/images/flags/united-states.png";
+  final String flagBrazil = "assets/images/flags/brazil.png";
+  final String flagSpain = "assets/images/flags/spain.png";
+
+  final String world = "assets/images/misc/world.png";
+  final String map = "assets/images/misc/map.png";
+}
+
+class Icons{
+  const Icons();
+
+  final String icUnitedStates = "assets/icons/united-states.png";
+  final String icBrazil = "assets/icons/brazil.png";
+  final String icSpain = "assets/icons/spain.png";
+}
